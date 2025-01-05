9c7bd24f14ec55bf710c7438e89098837f1e4365
simon <sirnomidea@gmail.com>
2025-01-05 16:18:23 +1300

    update tf variables

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,23 @@ output "processor_function_name" {
   description = "Name of the Lambda processor function"
   value       = module.processing.processor_function_name
 }
+
+output "media_bucket" {
+  value       = module.storage.media_bucket_name
+  description = "Name of the S3 bucket for media storage"
+}
+
+output "media_output_bucket" {
+  value       = module.storage.media_output_bucket_name
+  description = "Name of the S3 bucket for processed media output"
+}
+
+output "jobs_table" {
+  value       = module.storage.jobs_table_name
+  description = "Name of the DynamoDB table for jobs"
+}
+
+output "step_function_arn" {
+  value       = module.processing.step_function_arn
+  description = "ARN of the Step Function state machine"
+}
